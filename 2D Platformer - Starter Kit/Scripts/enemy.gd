d80af7a72ee6c6568034691a9d6f758ad304298a
kai <anisotas@gmail.com>
2023-10-19 17:32:36 -0700

    fixed broken enemy

--- 2D Platformer - Starter Kit/Scripts/enemy.gd
+++ 2D Platformer - Starter Kit/Scripts/enemy.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 
 var speed = 20
-var tween: = get_tree().create_tween()
+#var tween: = get_tree().create_tween()
 
 
 
 func _physics_process(delta):
-	tween.tween_property(self, "position.y", speed, 2.0).set_trans(Tween.TRANS_SINE)
+	#tween.tween_property(self, "position.y", speed, 2.0).set_trans(Tween.TRANS_SINE)
 	move_and_slide()
